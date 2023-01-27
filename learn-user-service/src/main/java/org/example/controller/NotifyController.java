65ff5e5cca22066331433dcc0438b6379863761d
yang <leon846666@gmail.com>
2023-01-27 18:05:16 +0800

    save validation code into redis in the docker from a server

--- learn-user-service/src/main/java/org/example/controller/NotifyController.java
+++ learn-user-service/src/main/java/org/example/controller/NotifyController.java
@@ -2,8 +2,11 @@ package org.example.controller;
 
 import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.example.utils.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Api(tags = "通知模块")
@@ -23,23 +27,54 @@ public class NotifyController {
 
     @Autowired
     private Producer captchaProducer;
+    /**
+     *临时使用10分钟有效，方便测试
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 60 * 1000 * 10;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    /**
+     * 获取图形验证码
+     * @param request
+     * @param response
+     */
+    @ApiOperation("获取图形验证码")
+    @GetMapping("captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response){
+
+        String cacheKey = getCaptchaKey(request);
 
 
-    @GetMapping("/getCaptcha")
-    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
-        String captchaProducerText = captchaProducer.createText();
-        log.info(" 图形验证码 :{}", captchaProducerText);
-        BufferedImage image = captchaProducer.createImage(captchaProducerText);
+        String capText = captchaProducer.createText();
+
+        //存储
+        redisTemplate.opsForValue().set(cacheKey,capText,CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
+
+        BufferedImage bi = captchaProducer.createImage(capText);
         ServletOutputStream out = null;
-
         try {
-            ServletOutputStream outputStream = response.getOutputStream();
-            ImageIO.write(image, "jpg", outputStream);
-            outputStream.flush();
-            outputStream.close();
+            response.setDateHeader("Expires", 0);
+            response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+            response.addHeader("Cache-Control", "create_date-check=0, pre-check=0");
+            response.setHeader("Pragma", "no-cache");
+            out = response.getOutputStream();
+            ImageIO.write(bi, "jpg", out);
+            out.flush();
+            out.close();
+
         } catch (IOException e) {
-            log.error("获取图形验证码异常 :{}",e);
+            log.error("获取验证码失败:{}",e);
         }
+    }
+
+    private String getCaptchaKey(HttpServletRequest request) {
+        String ip = CommonUtil.getIpAddr(request);
+        String header = request.getHeader("User-Agent");
+        log.info("user-agent: {}",header);
+        log.info("user-ip: {}",ip);
+        String key = "user-service:captcha:" + CommonUtil.MD5(ip + header);
+        return key;
     }
 
 
